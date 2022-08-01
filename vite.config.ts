b742ee0c3f48af63166125f8dd68198e60aa94ab
Zhihao Cui <davidcui2@me.com>
2022-08-01 22:19:12 +0100

    Bump vite and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,6 @@ export default defineConfig({
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
     cssCodeSplit: false,
-    brotliSize: false,
-    outDir: "../dist",
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => "everything.js",
-      },
-    },
+    outDir: "../dist"
   },
 });
