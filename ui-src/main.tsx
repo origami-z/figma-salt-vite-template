cacee327b54004925376952a380c4fedc77a975f
origami-z <davidcui2@me.com>
2022-11-16 09:33:50 +0000

    Bump uitk, simplify ui test config

--- ui-src/main.tsx
+++ ui-src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "@jpmorganchase/uitk-theme/index.css";
+import "@jpmorganchase/uitk-theme/css/global.css";
+import "@jpmorganchase/uitk-theme/css/theme.css";
 
 import App from "./App";
 
